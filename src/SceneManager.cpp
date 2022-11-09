8ba47e0f4328590d9d56495b54860b083fbf2636
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-11-09 17:38:48 +0100

    Created some error checking in SceneManager.

--- src/SceneManager.cpp
+++ src/SceneManager.cpp
@@ -1,20 +1,35 @@
+#include <raylib.h>
+
 #include "SceneManager.hpp"
 
 void SceneManager::LoadScene(Scene* scene, int id)
 {
-	if (id < SCENE_N)
-	{
-		m_loaded_scenes[id] = std::unique_ptr<Scene>(scene);
-		scene->sceneChangeEvent.Listen([this](int id) {
-			ChangeScene(id);
-		});
-	}
+	if (id >= SCENE_N)
+		return;
+
+	m_loaded_scenes[id] = std::unique_ptr<Scene>(scene);
+	scene->sceneChangeEvent.Listen([this](int id) {
+		ChangeScene(id);
+	});
 }
 
 void SceneManager::UpdateScene()
 {
+	if (!m_loaded_scenes[m_current_scene])
+		return;
+
 	m_loaded_scenes[m_current_scene]->Update();
-	m_loaded_scenes[m_current_scene]->Render();
+
+	if (!m_scene_change_requested)
+	{
+		BeginDrawing();
+			m_loaded_scenes[m_current_scene]->Render();
+		EndDrawing();
+	}
+	else
+	{
+		m_scene_change_requested = false;
+	}
 }
 
 void SceneManager::ChangeScene(int id)
@@ -24,6 +39,8 @@ void SceneManager::ChangeScene(int id)
 
 		m_current_scene = id;
 		m_loaded_scenes[id]->Start();
+
+		m_scene_change_requested = true;
 	}
 }
 
