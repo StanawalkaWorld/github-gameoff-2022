88dc1b6a8b568485a2c4c2b1a80e6f5700717faa
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-11-09 17:36:24 +0100

    Fixed EventEmitter not building.

--- include/EventEmitter.hpp
+++ include/EventEmitter.hpp
@@ -13,3 +13,16 @@ public:
 	void Emit(T val);
 };
 
+
+template <typename T>
+void EventEmitter<T>::Listen(Listener func) {
+	m_event_listeners.push_back(func);
+}
+
+template <typename T>
+void EventEmitter<T>::Emit(T val) {
+	for (const auto& f : m_event_listeners) {
+		f(val);
+	}
+}
+
